2c9bac0b218147047c539d6385a9734a92fd59c1
Loard <zaidangaming47@gmail.com>
2025-06-20 08:33:19 +0700

    Update delete.ts

--- pages/api/delete.ts
+++ pages/api/delete.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(403).json({ error: "Akses ilegal terdeteksi." });
+  res.status(403).json({
+    error: "Akses ilegal terdeteksi."
+  });
 }
+
 
